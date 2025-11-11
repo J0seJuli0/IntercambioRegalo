1b547ab2499054c69a86eadf488d8d6d1f801aa7
Jose Julio Sanchez Cruzado <josejuliosanchezcruzado1@gmail.com>
2025-11-11 17:40:44 +0000

    Try fixing this error: `Console Error: Error: Server returned: 404: <!DO

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,6 +1,5 @@
 import { genkit } from "@/ai/genkit";
 import { nextHandler } from "@genkit-ai/next";
-import "@/ai/flows/suggest-gift-ideas";
-import "@/ai/flows/generate-wishlist-from-description";
+import "@/ai/dev";
 
 export const POST = nextHandler(genkit);
