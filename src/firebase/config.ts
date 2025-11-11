601bdf56f88441282ddbec85520f825675c95732
Jose Julio Sanchez Cruzado <josejuliosanchezcruzado1@gmail.com>
2025-11-11 17:25:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3215978699-892ac",
+  "appId": "1:34148055679:web:bffc5088c21e48c3059d4a",
+  "apiKey": "AIzaSyDehYSR3FtYjCOxDt2w-yanqNbVc1UQgX4",
+  "authDomain": "studio-3215978699-892ac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "34148055679"
+};
